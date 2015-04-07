bbc652c75fea87bae6be3b668e04afb0eacb239f
Антон <admin@ideyniy.ru>
2015-04-07 16:30:56 +0300

    123

--- js/index.js
+++ js/index.js
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -59,15 +42,16 @@ var app = {
         scanner.scan( function (result) { 
 
             alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
+            "Результат: " + result.text + "\n" + 
+            "Формат: " + result.format + "\n" + 
+            "Отменен: " + result.cancelled);  
 
            console.log("Scanner result: \n" +
                 "text: " + result.text + "\n" +
                 "format: " + result.format + "\n" +
                 "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
+            infores = document.getElementById("info");
+            infores.innerHTML = 'Результат: ' + result.text;
             console.log(result);
             /*
             if (args.format == "QR_CODE") {
@@ -83,7 +67,7 @@ var app = {
     encode: function() {
         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
-        scanner.encode(scanner.Encode.TEXT_TYPE, "http://www.nhl.com", function(success) {
+        scanner.encode(scanner.Encode.TEXT_TYPE, "http://lakshmi.ru, function(success) {
             alert("encode success: " + success);
           }, function(fail) {
             alert("encoding failed: " + fail);
